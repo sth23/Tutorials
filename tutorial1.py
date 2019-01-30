b1c77545d33003ce3ecbbdaba7bfee6c38b88335
sth23 <47009471+sth23@users.noreply.github.com>
2019-01-30 14:03:59 -0500

    Updated from Brython Server: 1/30/2019 2:04:01 PM

--- tutorial1.py
+++ tutorial1.py
@@ -3,7 +3,7 @@ from ggame import App, Color, LineStyle, Sprite
 from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # Three primary colors with no transparency (alpha = 1.0)
-red = Color(0xff0000, 1.0)
+red = Color(0xff0000, 0.5)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
